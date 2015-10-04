49a7cb91dbe7efcb61e0f6329e182ceef4136588
jzamb <jesus.zambrano@360pi.com>
2015-10-04 18:34:49 -0400

    Remove secret_key from config file

--- config.py
+++ config.py
@@ -2,11 +2,6 @@ SQLALCHEMY_DATABASE_URI = (
     'mysql+pymysql://jzamb:3Dn}ht*AU2Uz@palindrome.c6rx6kxrdmgi.us-west-2'
     '.rds.amazonaws.com:3306/palindrome'
 )
-
 # Local DB.
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-
 SQLALCHEMY_POOL_RECYCLE = 3600
-
-# WTF_CSRF_ENABLED = True
-# SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
