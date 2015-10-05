c6be5a0fa1902a72e31aa89afdd8799c0b968e52
jzamb <jesus.zambrano@360pi.com>
2015-10-05 00:45:37 -0400

    Store db_uri in a constants file

---
+++ constants.py
@@ -0,0 +1,7 @@
+PALINDROME_DB_URI = 'sqlite:///test.db'
+
+# TODO use prod database
+# PALINDROME_DB_URI = (
+#     'mysql+pymysql://jzamb:3Dn}ht*AU2Uz@palindrome.c6rx6kxrdmgi.us-west-2'
+#     '.rds.amazonaws.com:3306/palindrome'
+# )
