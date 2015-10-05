2b52e032022911c0aae4e1a1ee8fdc1046606e49
jzamb <jesus.zambrano@360pi.com>
2015-10-05 01:31:49 -0400

    Start using rds service instead of local test db

--- constants.py
+++ constants.py
@@ -1,7 +1,4 @@
-PALINDROME_DB_URI = 'sqlite:///test.db'
-
-# TODO use prod database
-# PALINDROME_DB_URI = (
-#     'mysql+pymysql://jzamb:3Dn}ht*AU2Uz@palindrome.c6rx6kxrdmgi.us-west-2'
-#     '.rds.amazonaws.com:3306/palindrome'
-# )
+PALINDROME_DB_URI = (
+    'mysql+pymysql://jzamb:3Dn}ht*AU2Uz@palindrome.c6rx6kxrdmgi.us-west-2'
+    '.rds.amazonaws.com:3306/palindromedb'
+)
