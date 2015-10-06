45a08effa4ddff36dfe4f165abec22e9c354598c
jzamb <jzamb022@gmail.com>
2015-10-06 17:18:48 -0400

    Fist pass at documentation

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 from api import create_api
 from constants import PALINDROME_DB_URI, SECRET_KEY
 
@@ -15,6 +15,11 @@ def config_app():
         application.config['SECRET_KEY'] = SECRET_KEY
         application.config['SQLALCHEMY_DATABASE_URI'] = PALINDROME_DB_URI
         application.config['INITIALIZED'] = True
+
+
+@application.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
